891599df49c1ae89cd3277f8419f789fdace5b52
Kentaro Suzuki <kentaro84207@gmail.com>
2021-01-31 14:58:49 +0900

    SWR

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { getAllTasksData } from "../lib/tasks";
+import Task from "../components/Task";
+import useSWR from "swr";
+// import StateContextProvider from "../context/StateContext";
+// import TaskForm from "../components/TaskForm";
 
-export default function TaskPage({ filteredPosts }) {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticfilterdTasks }) {
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    initialData: staticfilterdTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
     <Layout title="Task page">
-      {/* <ul>
-      {filteredPosts &&
-        filteredPosts.map((post) => <Post key={post.id} post={post} />)}
-    </ul> */}
+      <ul>
+        {filteredTasks &&
+          filteredTasks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -29,4 +48,12 @@ export default function TaskPage({ filteredPosts }) {
       </Link>
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const staticfilterdTasks = await getAllTasksData();
+
+  return {
+    props: { staticfilterdTasks },
+    revalidate: 3,
+  };
 }
